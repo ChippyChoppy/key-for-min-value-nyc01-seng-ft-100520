84e0283b85062c959f3a96356dc0e08e521d8315
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 00:58:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,15 @@
 require 'pry'
 def key_for_min_value(name_hash)
     min_value = 10000
-    
+    min_key = nil 
     name_hash.each do |key, value|
     
     
     if value < min_value
       min_value = value
+      min_key = name_hash[key]
     end
-  name_hash[key] = value
-  key 
+  
   end
   binding.pry 
 end
