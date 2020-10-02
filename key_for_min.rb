db68be51f1c99bf21391bcda09b1d94a350b68e8
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 00:29:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@
 def key_for_min_value(name_hash)
     
     name_hash.each do |key, value|
-    min_value_key = name_hash[key][0]
-    if value < min_value_key
-      min_value_key = value
+    min_value = name_hash[key][0]
+    if value < min_value
+      min_value = value
     end
   end
 end
