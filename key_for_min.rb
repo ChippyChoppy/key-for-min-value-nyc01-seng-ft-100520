0b4de2fe2509ccaedbf501722d9c1b880b4d4fa6
Rebecca Robbins <rebeccamargaret13@gmail.com>
2020-10-02 00:16:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-require 'pry'
 def key_for_min_value(name_hash)
     
     name_hash.each do |key, value|
-      binding.pry
+
     min_value = name_hash[key][0]
     if value < min_value
       min_value = value
